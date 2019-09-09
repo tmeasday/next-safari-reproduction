482cf02956d89638e37cc6d8b0dcd22ec9caf029
Tom Coleman <tom@thesnail.org>
2019-09-09 15:26:59 +1000

    also works with google font url

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { createGlobalStyle } from 'styled-components';
 
 const CustomGlobalStyle = createGlobalStyle`
   body {
-    @import url('http://localhost:8080/fonts.css');
+    // @import url('http://localhost:8080/fonts.css');
+    @import url('https://fonts.googleapis.com/css?family=Nunito+Sans:400,700,800,900');
   }
 `;
 
